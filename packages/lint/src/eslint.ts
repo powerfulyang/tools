bc69ea4ece78c04739b660400a6e56b7190aabd9
powerfulyang <i@powerfulyang.com>
2020-10-12 23:51:24 +0800

    feat: add util getFileSuffix

--- packages/lint/src/eslint.ts
+++ packages/lint/src/eslint.ts
@@ -126,6 +126,7 @@ module.exports = {
     '@typescript-eslint/no-unused-expressions': 0,
     'no-await-in-loop': 0,
     'class-methods-use-this': 0,
+    'no-restricted-syntax': 0,
   },
   settings: {
     // support import modules from TypeScript files in JavaScript files
