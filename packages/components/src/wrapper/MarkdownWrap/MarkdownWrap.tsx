a81a6459685a543b026e8e35959dc62922115f60
powerfulyang <i@powerfulyang.com>
2020-11-22 02:28:38 +0800

    refactor(:white_flower:) new markdown component~

--- packages/components/src/wrapper/MarkdownWrap/MarkdownWrap.tsx
+++ packages/components/src/wrapper/MarkdownWrap/MarkdownWrap.tsx
@@ -1,11 +1,29 @@
 import React, { FC } from 'react';
 import ReactMarkdown from 'react-markdown';
-import gfm from 'remark-gfm';
+import remarkGfm from 'remark-gfm';
+import { BlockQuote, Code, Heading, Link, List, ListItem, Table } from './MarkdownElement';
+import styles from './index.scss';
 
 export type MarkdownWrapProps = {
   source: string;
 };
 
 export const MarkdownWrap: FC<MarkdownWrapProps> = ({ source }) => {
-  return <ReactMarkdown plugins={[gfm]}>{source}</ReactMarkdown>;
+  return (
+    <ReactMarkdown
+      className={styles.markdown_body}
+      plugins={[remarkGfm]}
+      renderers={{
+        code: Code,
+        heading: Heading,
+        link: Link,
+        blockquote: BlockQuote,
+        list: List,
+        listItem: ListItem,
+        table: Table,
+      }}
+    >
+      {source}
+    </ReactMarkdown>
+  );
 };
