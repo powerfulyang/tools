ffd00009efee2b6fb71b26f04c5f4750d3aac983
powerfulyang <i@powerfulyang.com>
2020-11-25 00:06:14 +0800

    feature(:yellow_heart:): admin page -> post blog page

--- packages/components/src/wrapper/MarkdownWrap/MarkdownElement.tsx
+++ packages/components/src/wrapper/MarkdownWrap/MarkdownElement.tsx
@@ -9,7 +9,7 @@ type CodeProps = {
   value: string;
   language: string;
 };
-export const Code: FC<CodeProps> = memo(({ value, language }) => {
+export const Code: FC<CodeProps> = memo(({ value = '', language }) => {
   const [showLanguage] = useState(language || 'javascript');
   return (
     <SyntaxHighlighter showLineNumbers language={showLanguage} style={style}>
@@ -43,11 +43,11 @@ const Head = (level: number, children: ReactNode) => {
     case HeadingEnum.H1:
       return <H1>{children}</H1>;
     case HeadingEnum.H2:
-      return <h2 className="text-3xl font-semibold my-2">{children}</h2>;
+      return <h2>{children}</h2>;
     case HeadingEnum.H3:
-      return <h3 className="text-2xl font-semibold my-2">{children}</h3>;
+      return <h3>{children}</h3>;
     case HeadingEnum.H4:
-      return <h4 className="text-xl font-semibold my-2">{children}</h4>;
+      return <h4>{children}</h4>;
     default:
       return children;
   }
@@ -59,7 +59,7 @@ export const Heading: FC<{ level: number }> = memo(({ level, children }) => {
 
 export const Link: FC<{ href: string }> = ({ href, children }) => {
   return (
-    <a className="font-medium text-blue-300 border-b-2" href={href}>
+    <a className="font-medium text-blue-600 border-b-2" href={href}>
       {children}
     </a>
   );
@@ -102,7 +102,7 @@ export const ListItem: FC<{ checked: null | boolean; ordered: boolean; index: nu
         </div>
       )}
       {props.ordered && (
-        <span className="text-blue-300 pr-2 text-xs font-medium">{props.index + 1}.</span>
+        <span className="text-blue-600 pr-2 text-sm font-medium">{props.index + 1}.</span>
       )}
       <span
         className={classNames(
