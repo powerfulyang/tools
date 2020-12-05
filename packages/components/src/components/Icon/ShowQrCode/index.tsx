5e150861e01a83533acd684441aee37dafc842dc
powerfulyang <i@powerfulyang.com>
2020-12-05 23:05:55 +0800

    fix: error with (inlineSourceMap vs sourceMap)

--- packages/components/src/components/Icon/ShowQrCode/index.tsx
+++ packages/components/src/components/Icon/ShowQrCode/index.tsx
@@ -5,9 +5,9 @@ type Props = {};
 
 export const ShowQrCode: FC<Props> = ({ children }) => {
   return (
-    <span className="pointer">
+    <>
       <Icon pointer onMouseEnter={() => {}} type="icon-ico" />
       {children}
-    </span>
+    </>
   );
 };
