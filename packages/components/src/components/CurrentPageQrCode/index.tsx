b3294bce07610b1e380f28d54388fd8ce3d2d7ea
powerfulyang <i@powerfulyang.com>
2020-12-08 00:49:10 +0800

    feat: user(ip) path view count.

--- packages/components/src/components/CurrentPageQrCode/index.tsx
+++ packages/components/src/components/CurrentPageQrCode/index.tsx
@@ -11,7 +11,7 @@ export const CurrentPageQrCode: FC<Props> = () => {
     const qr = new QrCode({
       value: window.location.href,
       size: 100,
-      level: 'Q',
+      level: 'H',
     });
     const base64 = qr.toDataURL();
     if (ref.current) {
@@ -19,22 +19,28 @@ export const CurrentPageQrCode: FC<Props> = () => {
       qrcode.src = base64;
       const ctx = ref.current.getContext('2d');
       ctx!.fillStyle = '#fff';
-      ctx!.fillRect(0, 0, 110, 110);
+      const bgRectWidth = 105;
+      const offset = (bgRectWidth - bgRectWidth) / 2;
+      ctx!.fillRect(offset, offset, bgRectWidth, bgRectWidth);
       qrcode.onload = () => {
-        ctx!.drawImage(qrcode, 5, 5, 100, 100);
+        const qrCodeWith = 95;
+        const offset1 = (bgRectWidth - qrCodeWith) / 2;
+        ctx!.drawImage(qrcode, offset1, offset1, qrCodeWith, qrCodeWith);
       };
       const avatar = new Image();
       avatar.src =
         'https://lh3.googleusercontent.com/a-/AOh14GhPb7zfSSYTvNA6psX28gfeiA5br7m1iPhwq72_-Q=s96-c';
       avatar.onload = () => {
-        ctx!.drawImage(avatar, 35, 35, 30, 30);
+        const avatarWith = 30;
+        const offset2 = (bgRectWidth - avatarWith) / 2;
+        ctx!.drawImage(avatar, offset2, offset2, avatarWith, avatarWith);
       };
     }
   }, []);
 
   return (
     <div className={styles.shareQrCode}>
-      <canvas ref={ref} width={110} height={110} />
+      <canvas ref={ref} width={105} height={105} />
     </div>
   );
 };
