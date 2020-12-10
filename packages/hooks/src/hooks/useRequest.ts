f841ba52941c6b6ab1c76758c0a19127b1e83190
powerfulyang <i@powerfulyang.com>
2020-12-10 16:49:32 +0800

    feat: more usable useRequest.

--- packages/hooks/src/hooks/useRequest.ts
+++ packages/hooks/src/hooks/useRequest.ts
@@ -1,5 +1,5 @@
 import { stringify } from 'qs';
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect, useMemo, useRef } from 'react';
 import { Subject } from 'rxjs';
 import { ReqOptions } from '../type/ReqOptions';
 import { useImmer } from './useImmer';
@@ -14,6 +14,7 @@ export const useRequest = <T = any>(reqOptions: ReqOptions<T>) => {
   const { url, body, query, runCase = true } = reqOptions;
   const { immediately = true, defaultResponse, resTransform = defaultResTransform } = cachedOptions;
   const [res, setRes] = useImmer(defaultResponse);
+  const [loading, setLoading] = useImmer(false);
   let reqUrl = '';
   if (!url.startsWith('http')) {
     reqUrl = `${UseRequest.baseUrl}${url}`;
@@ -22,16 +23,10 @@ export const useRequest = <T = any>(reqOptions: ReqOptions<T>) => {
     reqUrl = `${reqUrl}?${stringify(query)}`;
   }
   const isFirstRef = useRef(0);
+  const subject = useMemo(() => new Subject(), []);
   useEffect(() => {
-    const subject = new Subject();
-    if ((immediately || isFirstRef.current > 0) && runCase) {
-      const response = fetch(reqUrl);
-      response
-        .then(resTransform)
-        .then((r) => subject.next(r))
-        .catch((e) => subject.error(e));
-    }
-    subject.subscribe(
+    setLoading(true);
+    const subscription = subject.subscribe(
       (r: T) => {
         setRes(r);
       },
@@ -39,10 +34,20 @@ export const useRequest = <T = any>(reqOptions: ReqOptions<T>) => {
         setRes(e);
       },
     );
+    if ((immediately || isFirstRef.current > 0) && runCase) {
+      const response = fetch(reqUrl);
+      response
+        .then(resTransform)
+        .then((r) => subject.next(r))
+        .catch((e) => subject.error(e))
+        .finally(() => {
+          setLoading(false);
+        });
+    }
     isFirstRef.current++;
     return () => {
-      subject.unsubscribe();
+      subscription.unsubscribe();
     };
-  }, [body, immediately, reqUrl, resTransform, runCase, setRes]);
-  return [res] as const;
+  }, [body, immediately, reqUrl, resTransform, runCase, setLoading, setRes, subject]);
+  return [loading, res] as const;
 };
--- packages/hooks/src/test/useRequest.spec.ts
+++ packages/hooks/src/test/useRequest.spec.ts
@@ -1,16 +1,36 @@
 import { renderHook } from '@testing-library/react-hooks';
+import { Subject } from 'rxjs';
 import { UseRequest, useRequest } from '..';
 
 describe('useRequest', () => {
   it('should ', (done) => {
+    const subject = new Subject();
     UseRequest.baseUrl = 'https://api.powerfulyang.com';
-    const { result } = renderHook(() => {
-      return useRequest({ url: '/api/posts/1' });
-    });
-    setTimeout(() => {
-      const data = result.current[0];
-      expect(data).toHaveProperty('status', 'ok');
-      done();
-    }, 500);
+
+    const requestHooks = renderHook(
+      (id: number) => {
+        return useRequest({
+          url: `/api/posts/${id}`,
+          async resTransform(res) {
+            const result = await res.json();
+            subject.next(result);
+          },
+        });
+      },
+      { initialProps: 1 },
+    );
+
+    subject.subscribe(
+      (data: any) => {
+        expect(data).toHaveProperty('status', 'ok');
+        if (data.data.id === 2) {
+          subject.error(0);
+        }
+        requestHooks.rerender(2);
+      },
+      () => {
+        done();
+      },
+    );
   });
 });
