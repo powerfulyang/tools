b4c4c72c6aacf0cea4367a81e9165c0151566b27
powerfulyang <i@powerfulyang.com>
2020-12-10 14:23:08 +0800

    chore: remove table page

--- packages/hooks/src/test/useRequest.spec.ts
+++ packages/hooks/src/test/useRequest.spec.ts
@@ -2,13 +2,15 @@ import { renderHook } from '@testing-library/react-hooks';
 import { UseRequest, useRequest } from '..';
 
 describe('useRequest', () => {
-  it('should ', () => {
+  it('should ', (done) => {
     UseRequest.baseUrl = 'https://api.powerfulyang.com';
     const { result } = renderHook(() => {
       return useRequest({ url: '/api/posts/1' });
     });
     setTimeout(() => {
-      expect(result.current[0]).toHaveProperty('status', 'ok');
+      const data = result.current[0];
+      expect(data).toHaveProperty('status', 'ok');
+      done();
     }, 500);
   });
 });
