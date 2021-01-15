11347435232cfd796e1e86bd6ce062c87669869a
powerfulyang <i@powerfulyang.com>
2021-01-15 18:16:09 +0800

    perf: components

--- packages/components/src/components/index.ts
+++ packages/components/src/components/index.ts
@@ -3,5 +3,4 @@ export * from './Icon';
 export * from './Icon/IconTag';
 export * from './Icon/ShowQrCode';
 export * from './Tooltip';
-export * from './Popover';
 export * from './CurrentPageQrCode';
